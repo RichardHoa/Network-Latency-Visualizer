89b26e77113455aa0c12ca31fa5121c825f7c367
RichardHoa <richardhoa.work@gmail.com>
2024-10-12 21:15:14 +0700

    adding function to check user input and complete function to set up cronjob

--- main.go
+++ main.go
@@ -5,66 +5,123 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strings"
+	// "time"
+
 	"github.com/joho/godotenv"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	WORKING_DIR := "/Users/hoathaidang/Documents/bootdev/go-networking"
+	fmt.Printf("Working dir is: %s\n", WORKING_DIR)
 
-	err := godotenv.Load()
+	setupCronJobErr := godotenv.Load()
 
-	if err != nil {
+	if setupCronJobErr != nil {
 		log.Fatal("Error loading .env file")
 	}
-	
+
 	fmt.Println("Welcome to the Network-Latency-Visualizer!")
 
-	
+	fmt.Println("For the first step, please choose how often you want to check your network latency")
+	fmt.Println("Minimum: 1 minutes, maximum: 1 day")
 
-	crontabJobs, err := exec.Command("crontab", "-l").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	crontabArray := string(crontabJobs)
-	if strings.Contains(crontabArray, "networking") {
-		fmt.Println("Crontab has been established")
-	}
+	timeInMinutes := askForTimeInput()
 
-	reportWD := WORKING_DIR + "/report/report.txt"
+	fmt.Printf("You chose %d minutes\n", timeInMinutes)
 
-	file, openFileErr := os.OpenFile(reportWD, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
-	if openFileErr != nil {
-		log.Fatal(openFileErr)
-	}
-
-	defer file.Close()
-
-
-	data := []byte("1 * * * * /Users/hoathaidang/Documents/bootdev/go-networking/networking\n")
-
-	_, writeFileErr := file.Write(data)
-	if writeFileErr != nil {
-		log.Fatal(writeFileErr)
-	}
-	fmt.Println("Write file successfully")
-	fmt.Printf("File directory is %s\n", reportWD)
-
-
-
-	// var lengthInMinutes int
-	// fmt.Println("For the first step, please choose how often you want to check your network latency")
-	// fmt.Println("Minimum: 1 minutes, maximum: 1 day (86400 minutes)")
-	// fmt.Printf("Please type the number in minutes: ")
-	// fmt.Scan(&lengthInMinutes)
-
-	// output, err := exec.Command("ping", "google.com", "-c", "10").Output()
-	// if err != nil {
-	// 	log.Fatal(err)
+	// output, setupCronJobErr := exec.Command("ping", "google.com", "-c", "10").Output()
+	// if setupCronJobErr != nil {
+	// 	log.Fatal(setupCronJobErr)
 	// }
 
 	// outputString := string(output)
 	// outputArray := strings.Split(outputString, "\n")
 	// fmt.Println(outputArray[len(outputArray)-2])
-	// fmt.Printf("The length is %d minutes\n", lengthInMinutes)
+
+	// err := setUpCronJob("1 * * * *", WORKING_DIR)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+}
+
+func setUpCronJob(timeString string, WORKING_DIR string) error {
+
+	cronjobWorkingDir := WORKING_DIR + "/cronjob/cron.txt"
+
+	crontabJobs, setupCronJobErr := exec.Command("crontab", "-l").Output()
+	if setupCronJobErr != nil {
+		return setupCronJobErr
+	}
+
+	file, openFileErr := os.OpenFile(cronjobWorkingDir, os.O_RDWR|os.O_CREATE, 0777)
+	if openFileErr != nil {
+		return openFileErr
+	}
+
+	defer file.Close()
+
+	cronjob := "* * * * * " + WORKING_DIR + "/networking" + "\n"
+
+	_, writeExistingCronJobErr := file.Write(crontabJobs)
+	if writeExistingCronJobErr != nil {
+		return writeExistingCronJobErr
+	}
+
+	_, writeNewCronJobErr := file.WriteString(cronjob)
+	if writeNewCronJobErr != nil {
+		return writeNewCronJobErr
+	}
+
+	setupCronJob := exec.Command("crontab", cronjobWorkingDir)
+
+	setupCronJobErr = setupCronJob.Run()
+	if setupCronJobErr != nil {
+		return setupCronJobErr
+	}
+
+	return nil
+}
+
+func calculateTimeString(timeInMinutes int) string {
+	return ""
+
+}
+
+func askForTimeInput() (timeInMinutes int) {
+	var time int
+
+	for {
+		var inputTime string
+		var timeMark string
+		var min int
+		fmt.Println("Example input: 1 mins, 5 mins, 1 hrs, 15 hrs.")
+		fmt.Printf("Your chosen time is: ")
+
+		fmt.Scanf("%s %s", &inputTime, &timeMark)
+
+		if strings.Contains(timeMark, "mins") {
+			minString := strings.Split(inputTime, "mins")[0]
+			min, _ = strconv.Atoi(minString)
+		} else if strings.Contains(timeMark, "hrs") {
+			minString := strings.Split(inputTime, "hrs")[0]
+			hours, _ := strconv.Atoi(minString)
+			min = hours * 60
+		} else {
+			fmt.Println("It's either minutes or hours")
+			continue
+		}
+
+		if min >= 1 && min <= 86400 {
+			time = min
+			break
+		} else {
+			fmt.Println("Please remember the maximum time")
+		}
+
+	}
+
+	return time
 }
